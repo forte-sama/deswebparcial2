c23b946408884daa65b1addcc062cbb24f6578dd
Neury A Frias <neury0102@gmail.com>
2016-07-01 14:58:43 -0400

    publicacion filtros

--- src/main/java/main/ManejoFiltros.java
+++ src/main/java/main/ManejoFiltros.java
@@ -4,7 +4,9 @@ package main;
  * Created by Dell_2 on 6/29/2016.
  */
 
+import modelos.Publicacion;
 import modelos.Usuario;
+import servicios.PublicacionServicios;
 
 import static spark.Spark.*;
 public class ManejoFiltros {
@@ -43,6 +45,16 @@ public class ManejoFiltros {
                 response.redirect("/");
             }
         });
+
+        before("/publicacion/editar/:valor/",(request, response) -> {
+            Usuario usuario=request.session().attribute("usuario");
+            Publicacion publicacion= PublicacionServicios.getInstancia().find(Integer.parseInt(request.params("valor")));
+            if(usuario == null||usuario.getAutorizado()==false|| !usuario.getUsername().equals(publicacion.getUsuario().getUsername())){
+                response.redirect("/");
+            }
+        });
+
+
 
 
 
